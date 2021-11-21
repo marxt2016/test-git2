86a2513a9868fa528de2779010741980789047f4
Olga Melnikova <marxt@yandex.ru>
2021-11-21 09:25:35 +0400

    made chages in feature

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ console.log("Hello world");
 console.log("Hello");
 
 console.log("Error");
+
+const age = 20;
+if (age > 18) {
+    console.log('more 18');
+} else {
+    console.log('less 18');
+}
